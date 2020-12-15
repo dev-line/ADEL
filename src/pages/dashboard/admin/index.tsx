bcbeffb1f86dc19f9a56bd1a6b359393956c6e19
root <safsystm@gmail.com>
2020-12-15 13:07:55 +0100

    final

--- src/pages/dashboard/admin/index.tsx
+++ src/pages/dashboard/admin/index.tsx
@@ -4,69 +4,37 @@ import DASBOILERPLATE from '../../../components/DASBOILERPLATE'
 export default function index() {
     return (
         <DASBOILERPLATE title="الرئيسية">
-            <div className="col-md-10">
-                <div className="container spacer-70">
-
-                    <div className="row card-budget">
-                        {/* <!-- Card 01 --> */}
-                        <div className="col-6 col-lg-3 px-2 mb-3 mb-lg-0">
-                            <div className="card shadow-lg rounded-lg">
-                                <div className="card-body text-center">
-                                    <div className="mb-3">
-                                        <div className="icon-shape bg-primary-f shadow-primary text-white">
-                                            <i className="fal fa-file-alt"></i>
-                                        </div>
-                                    </div>
-                                    <h5 className="h3 font-weight-bolder text-muted-f mb-1">65</h5>
-                                    <a href="./allPosts.html" className="d-block small text-muted font-weight-bold stretched-link">المقالات</a>
-                                </div>
-                            </div>
-                        </div>
-                        {/* <!-- Card 02 --> */}
-                        <div className="col-6 col-lg-3 px-2 mb-3 mb-lg-0">
-                            <div className="card shadow-lg rounded-lg">
-                                <div className="card-body text-center">
-                                    <div className="mb-3">
-                                        <div className="icon-shape bg-danger-f shadow-danger text-white">
-                                            <i className="fal fa-comment-lines"></i>
-                                        </div>
-                                    </div>
-                                    <h5 className="h3 font-weight-bolder text-muted-f mb-1">670</h5>
-                                    <a href="./comments.html" className="d-block small text-muted font-weight-bold stretched-link">التعليقات</a>
-                                </div>
-                            </div>
-                        </div>
-                        {/* <!-- Card 03 --> */}
-                        <div className="col-6 col-lg-3 px-2 mb-3 mb-lg-0">
-                            <div className="card shadow-lg rounded-lg">
-                                <div className="card-body text-center">
-                                    <div className="mb-3">
-                                        <div className="icon-shape bg-warning-f shadow-warning text-white">
-                                            <i className="fal fa-user-friends"></i>
-                                        </div>
-                                    </div>
-                                    <h5 className="h3 font-weight-bolder text-muted-f mb-1">5</h5>
-                                    <a href="./members.html" className="d-block small text-muted font-weight-bold stretched-link">الأعضاء</a>
-                                </div>
-                            </div>
-                        </div>
-                        {/* <!-- Card 04 --> */}
-                        <div className="col-6 col-lg-3 px-2 mb-3 mb-lg-0">
-                            <div className="card shadow-lg rounded-lg">
-                                <div className="card-body text-center">
-                                    <div className="mb-3">
-                                        <div className="icon-shape bg-success-f shadow-success text-white">
-                                            <i className="fal fa-inbox"></i>
-                                        </div>
-                                    </div>
-                                    <h5 className="h3 font-weight-bolder text-muted-f mb-1">163</h5>
-                                    <a href="inbox.html" className="d-block small text-muted font-weight-bold stretched-link">الرسائل</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+ <div className="col-md-10">
+        <div className="container vh-100 content-centered">
+          <div className="d-lg-flex flex-lg-column w-100">
+            <div className="d-block">
+              <div className="w-md-75 w-lg-50 mx-auto">
+                <img
+                  className="img-fluid"
+                  src="/assets/img/icons/svg/security.svg"
+                  alt="Image Description"
+                />
+              </div>
+              <div className="w-md-75 mx-auto mt-5 text-center">
+                <h2>
+                  Do it better, fancy &amp; modern with
+                  <a
+                    href="https://linktr.ee/hax.codes"
+                    target="_blank"
+                    className="text-secondary-f"
+                  >
+                    Hax Codes<span>&trade;</span>.
+                  </a>
+                </h2>
+                <p className="w-100 w-md-65 mx-auto font-size-2 mb-0">
+                  We provide content-oriented, clean, modular and mobile-first
+                  design, which you can easily customize.
+                </p>
+              </div>
             </div>
+          </div>
+        </div>
+      </div>
         </DASBOILERPLATE>
     )
 }
